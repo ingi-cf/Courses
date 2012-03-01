dc673c906b6d614a41b48a52b76b09018679bb3a
frol2103 <frol2103@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2012-03-01 09:47:47 +0000

    git-svn-id: http://subversion.assembla.com/svn/eplsvn/2011-2012@159 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- LINGI2263-Computational_linguistics/Assignment 1/script/mt_extractor.py
+++ LINGI2263-Computational_linguistics/Assignment 1/script/mt_extractor.py
@@ -10,21 +10,26 @@ scriptDir = os.path.abspath(os.path.join(sys.argv[0], os.path.pardir))
 
 def execUnitex(src,unitexAppFolder):
     base =  os.path.abspath(src)[:-4]
-    unitexToolLogger    = unitexAppFolder + "UnitexToolLogger"
-    u_norm              = scriptDir + "/ressources/Norm.txt"
-    u_alphabet          = scriptDir + "/ressources/Alphabet.txt"
+    ressources = os.path.join(scriptDir,'ressources')
+    unitexToolLogger    = os.path.join(unitexAppFolder,"UnitexToolLogger")
+    u_norm              = os.path.join(ressources, "Norm.txt")
+    u_alphabet          = os.path.join(ressources, "Alphabet.txt")
     d_snt               = base + "_snt"
     f_snt               = base + ".snt"
     dst                 = base + ".tmp.txt"
-    d_graph             = scriptDir + "/ressources/graphs"
+    d_graph             = os.path.join(ressources, "graphs")
+
+    u_concord           = os.path.join(d_snt, 'concord.ind')
+    u_graph             = os.path.join(d_graph,'all.grf')
+    u_graph2            = os.path.join(d_graph,'all.fst2')
 
     cmd = []
     cmd.append("\"" + unitexToolLogger + "\" Normalize \"" + src + "\" \"-r" + u_norm + "\"")
     cmd.append("mkdir \"" + d_snt + "\"")
     cmd.append("\"" + unitexToolLogger + "\" Tokenize \"" + f_snt + "\" \"-a" + u_alphabet + "\"")
-    cmd.append("\"" + unitexToolLogger + "\" Grf2Fst2 \"" + d_graph + "/all.grf\" -y \"--alphabet=" + u_alphabet + "\"")
-    cmd.append("\"" + unitexToolLogger + "\" Locate \"-t" + f_snt + "\" \"" + d_graph + "/all.fst2\" \"-a" + u_alphabet + "\" -L -M --all -b -Y")
-    cmd.append("\"" + unitexToolLogger + "\" Concord \"" + d_snt + "/concord.ind\" \"-m" + dst + "\" ")
+    cmd.append("\"" + unitexToolLogger + "\" Grf2Fst2 \"" + u_graph + "\" -y \"--alphabet=" + u_alphabet + "\"")
+    cmd.append("\"" + unitexToolLogger + "\" Locate \"-t" + f_snt + "\" \"" + u_graph2 + "\" \"-a" + u_alphabet + "\" -L -M --all -b -Y")
+    cmd.append("\"" + unitexToolLogger + "\" Concord \"" + u_concord + "\" \"-m" + dst + "\" ")
     
     for c in cmd:
         os.system(c)
@@ -39,13 +44,13 @@ inputDir = sys.argv[1]
 outputDir = sys.argv[2]
 
 if len(sys.argv) > 3:
-    unitexPath = sys.argv[3] + "/"
+    unitexPath = sys.argv[3]
 else:
     unitexPath = "" 
 
 for f in os.listdir(inputDir):
-    src = inputDir + "/" + f
-    dst = outputDir + "/" + f
+    src = os.path.join(inputDir, f)
+    dst = os.path.join(outputDir , f)
 
     if(not os.path.isdir(src) and src[-4:] == ".txt"):
         tmpFile = execUnitex(src,unitexPath)
