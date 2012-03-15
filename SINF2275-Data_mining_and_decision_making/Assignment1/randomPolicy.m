99b9eeda9d91d19a1da09c3c6514b14172759e43
elyndir <elyndir@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2012-03-15 16:04:28 +0000

    git-svn-id: http://subversion.assembla.com/svn/eplsvn/2011-2012@216 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ SINF2275-Data_mining_and_decision_making/Assignment1/randomPolicy.m
@@ -0,0 +1,15 @@
+function ran = randomPolicy(List)
+%RANDOMPOLICY Summary of this function goes here
+%   Detailed explanation goes here
+
+ran = 2.*rand(1,length(List))
+
+for(i=1:length(ran))
+    if ran(i)>1
+        ran(i)=1
+    else
+        ran(i)=2
+    end
+end
+end
+
