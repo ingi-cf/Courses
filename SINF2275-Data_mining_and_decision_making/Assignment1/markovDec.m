2ee19c3874df3546f7488ed8d14231329dae130d
elyndir <elyndir@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2012-03-16 00:18:12 +0000

    git-svn-id: http://subversion.assembla.com/svn/eplsvn/2011-2012@227 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- SINF2275-Data_mining_and_decision_making/Assignment1/markovDec.m
+++ SINF2275-Data_mining_and_decision_making/Assignment1/markovDec.m
@@ -3,8 +3,10 @@ function [Expec, Dice] = markovDec(List)
 %   List represent the traps types
 %   The board used is the basic board with no loop (passing arrival=win)
 
-map.s0     = 1;
-map.d      = 11;
+map.s0     = 1; %initial square
+map.d      = 11; %goal square
+
+%affinity matrix
 map.links  = [ 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0;
                 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0;
                 0 0 0 1 0 0 0 0 0 0 0 2 0 0 0;
@@ -20,7 +22,7 @@ map.links  = [ 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0;
                 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0;
                 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1;
                 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0];
-            
+%traps types            
 map.traps = List;
 
 [Dice, Expec] = snake(map);
