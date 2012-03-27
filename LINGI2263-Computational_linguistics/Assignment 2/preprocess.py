e84caa7ab83bbce1c65dff0c773b58e9f0642c10
Elyndir <elyndir@gmail.com>
2012-03-27 21:54:27 +0200

    preprocess now writes tokenized files

--- LINGI2263-Computational_linguistics/Assignment 2/preprocess.py
+++ LINGI2263-Computational_linguistics/Assignment 2/preprocess.py
@@ -1,26 +1,58 @@
 import re
+import os
+import glob
+import sys
 
+#get source files folder
+input = "blogs"
+output = "tokenized"
+if(len(sys.argv)>1):
+    input = sys.argv[1]
+if(len(sys.argv)>2):
+    output = sys.argv[2]
+    
 def tokenize(string):
     patterns = []
     patterns.append("[\w\-]+") #any word
-    patterns.append("\'s") #'s
-    patterns.append("[:()@pPDdxXsS-_^]{2,3}") #smileys
-    patterns.append("[0-9=\-*\/+%()]+") #mathematical exp or date
+    patterns.append("\'s|\'t|\'m") #is (or gen), not and am
+    patterns.append("[:()@pPDdxXsS-_^\[\]]{2,3}") #smileys
+    patterns.append("[0-9=\-*\/+%()^]+") #mathematical exp or date
     patterns.append("[a-z._]+@[a-z.]+.[a-z]{2,3}") #mail adress
     return re.findall("|".join(patterns),string)
 
 
-file = open("blogs/Blogs_F_train.txt", "r")
+
 lexicon={}
 
-#tokenize each line
-for line in file:
-    tokens = tokenize(line)
-    for t in tokens:
-        if(t in lexicon):
-            lexicon[t] = lexicon[t] +1
-        else:
-            lexicon[t] = 1
+#read all training files
+for filename in glob.glob( os.path.join(input, '*train.txt')):
+    file = open(filename, "r")
+
+    tokenized_file = []
+    #tokenize each line
+    for line in file:
+        tokens = tokenize(line)
+        tokenized_file.append(tokens)
+        for t in tokens:
+            if(t in lexicon):
+                lexicon[t] = lexicon[t] +1
+            else:
+                lexicon[t] = 1
+    file.close()
+    
+    #write tokenized sentences to another file
+    m = re.search("(?<="+input+").+",filename)
+    outfilename = output+m.group()
+    outfile = open(outfilename, "w")
+    
+    for line in tokenized_file:
+        for tok in line:
+            outfile.write(str(tok)+" ")
+        outfile.write("\n")
+    
+    outfile.close()
+    
+    
 
 #obtain 20 most used tokens
 lexicon_sorted = sorted(lexicon.items(), key=lambda pair:pair[1], reverse=True)
