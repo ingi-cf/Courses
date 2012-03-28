e6e9beece70807b94eaeea62619321cf620feeee
Elyndir <elyndir@gmail.com>
2012-03-28 13:31:04 +0200

    ngrams nows writes to ngrams_data.txt the number of ngrams for each frequency

--- LINGI2263-Computational_linguistics/Assignment 2/ngrams.py
+++ LINGI2263-Computational_linguistics/Assignment 2/ngrams.py
@@ -30,8 +30,33 @@ def countNgrams(tokens, n):
             lexicon[gram] = 1
     return sorted(lexicon.items(), key=lambda pair:pair[1], reverse=True)
     
-for i in range(1,4):
-    lexicon_sorted = countNgrams(tokens,i)
+
+
+
+counts = {}
+for i in range(0,3):
+    lexicon_sorted = countNgrams(tokens,i+1)
     print("top 20 "+str(i)+"-grams : ")
+    
     for lex in lexicon_sorted[0:20]:
         print(str(lex[0]) + "  :  " +str(lex[1]))
+    for lex in lexicon_sorted:
+        if lex[1] in counts:
+            counts[lex[1]][i] = counts[lex[1]][i]+1
+        else:
+            counts[lex[1]] = [0,0,0]
+            counts[lex[1]][i] = 1
+    
+
+
+#write number of grams(count) in file
+file = open("ngrams_data.txt", "w")
+for key in sorted(counts.keys()):
+    file.write(str(key)+"\t"+str(counts[key][0])+"\t"+str(counts[key][1])+"\t"+str(counts[key][2])+"\n")
+
+file.close()
+
+
+
+    
+    
