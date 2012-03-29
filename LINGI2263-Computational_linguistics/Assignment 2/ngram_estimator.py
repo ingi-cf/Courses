65393a6c7f48467555cf1ab5b52c974b9b71c3cc
Elyndir <elyndir@gmail.com>
2012-03-29 04:37:59 +0200

    perplexity done smoothing to do again

--- LINGI2263-Computational_linguistics/Assignment 2/ngram_estimator.py
+++ LINGI2263-Computational_linguistics/Assignment 2/ngram_estimator.py
@@ -198,29 +198,7 @@ class NgramEstimator:
         else:
             return 'undetermined'
                 
-#get models, can take some time
-testE = NgramEstimator("tokenized",5)
 
-testE.computeLambdas()
-"""
-print(testE.flambdas)
-print(testE.mlambdas)
-
-if testE.consistencyLap('male',5):
-    print("male model is consistent with laplace smoothing")
-if testE.consistencyLap('female',5):
-    print("female model is consistent with laplace smoothing")
-"""
-
-#read test file and classifies lines
-filename = "tokenized/Blogs_M_test.txt"
-file = open(filename, "r")
-for i in range(0,10):
-    line = file.readline().rstrip('\n ')
-    
-    print("perplexM "+str(testE.perplexity(line, 'male'))+"perplexF "+str(testE.perplexity(line, 'female'))+" from "+filename+" as "+testE.predictLap(line))
-
-file.close()
 
 
     
