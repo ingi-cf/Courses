88654b7d08f1313b6e4b708ccf717ea3180886a9
François Pelsser <git@frol.be>
2012-03-30 17:42:30 +0200

    add element to instantiate class

--- LINGI2132-Langages_et_traducteurs/interpreter/postal/environment/ClassesContainer.java
+++ LINGI2132-Langages_et_traducteurs/interpreter/postal/environment/ClassesContainer.java
@@ -1,5 +1,13 @@
 package postal.environment;
 
+import postal.classes.PostalClass;
+
 public class ClassesContainer {
+
+	public PostalClass getClasse(String className) {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- LINGI2132-Langages_et_traducteurs/interpreter/postal/environment/PostalEnvironment.java
+++ LINGI2132-Langages_et_traducteurs/interpreter/postal/environment/PostalEnvironment.java
@@ -1,18 +1,26 @@
 package postal.environment;
 
 import postal.ast.ElementNode;
+import postal.objects.MessageObject;
 import postal.objects.PostalObject;
 
 public class PostalEnvironment {
+	
+	VariablesContainer vc;
+	ClassesContainer cc;
 
 	public PostalObject getVariable(String identifier) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 	
 	public void setVariable(String identifier, ElementNode element) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public PostalObject newObject(String className, MessageObject newMessage)
+	{
+		return cc.getClasse(className).messageReceived(null, newMessage);
 	}
 
 }
