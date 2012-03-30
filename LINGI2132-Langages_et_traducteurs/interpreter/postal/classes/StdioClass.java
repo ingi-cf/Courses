53922b782e2fcc754bcc148067780dee2c30151c
Nicolas Forêt <nicolas4@gmail.com>
2012-03-30 12:19:59 +0200

    Ajout (pour de bon) de StdioClass

---
+++ LINGI2132-Langages_et_traducteurs/interpreter/postal/classes/StdioClass.java
@@ -0,0 +1,24 @@
+package postal.classes;
+
+import postal.exceptions.TypeException;
+import postal.objects.MessageObject;
+import postal.objects.PostalObject;
+import postal.objects.StdioObject;
+
+public class StdioClass extends PostalClass
+{
+    public PostalObject messageReceived(PostalObject o, MessageObject m)
+    {
+    	assert(o instanceof StdioObject);
+    	StdioObject o1 = (StdioObject) o;
+        if(m.name().equals("print"))
+        {
+        	o1.print(m.param(0));
+        	return null;        	
+        }
+        else
+        {
+            throw new TypeException("BooleanClass: Message not known.");
+        }
+    }
+}
