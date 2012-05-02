e5ae42548de7ff6fbbc5fc50a78c86ea8c6ed4f4
Elyndir <elyndir@gmail.com>
2012-05-02 18:39:41 +0200

    updated filter pos tags list and addded to report

--- LINGI2263-Computational_linguistics/Assignment 3/script/preprocess/filter.py
+++ LINGI2263-Computational_linguistics/Assignment 3/script/preprocess/filter.py
@@ -4,8 +4,8 @@ source_file_name = "tagged_definitions-6914.csv"
 if(len(sys.argv)>1):
     source_file_name = sys.argv[1]
     
-stopwords = ["a","able","about","across","after","all","almost","also","am","among","an","and","any","are","as","at","be","because","been","but","by","can","cannot","could","dear","did","do","does","either","else","ever","every","for","from","get","got","had","has","have","he","her","hers","him","his","how","however","i","if","in","into","is","it","its","just","least","let","like","likely","may","me","might","most","must","my","neither","no","nor","not","of","off","often","on","only","or","other","our","own","rather","said","say","says","she","should","since","so","some","than","that","the","their","them","then","there","these","they","this","tis","to","too","twas","us","wants","was","we","were","what","when","where","which","while","who","whom","why","will","with","would","yet","you","your"]
-stopposes = ["DT", "SENT"]
+stopwords = [",", ";", "a","able","about","across","after","all","almost","also","am","among","an","and","any","are","as","at","be","because","been","but","by","can","cannot","could","dear","did","do","does","either","else","ever","every","for","from","get","got","had","has","have","he","her","hers","him","his","how","however","i","if","in","into","is","it","its","just","least","let","like","likely","may","me","might","most","must","my","neither","no","nor","not","of","off","often","on","only","or","other","our","own","rather","said","say","says","she","should","since","so","some","than","that","the","their","them","then","there","these","they","this","tis","to","too","twas","us","wants","was","we","were","what","when","where","which","while","who","whom","why","will","with","would","yet","you","your"]
+stopposes = ["DT", "SENT", ",", "IN", "CC"]
 
 def filter(source_file_name, stopwords, stopposes):
     dest_file_name = "filtered_"+source_file_name
