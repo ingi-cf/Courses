26a3af3e7bf88481d3bd74cf8d80a35e21d4bfbc
François Pelsser <frol@Joshua.(none)>
2012-05-18 01:25:32 +0200

    adding tests

---
+++ LINGI2132-Langages_et_traducteurs/interpreter/test2.java
@@ -0,0 +1,33 @@
+import postal.parser.Executer;
+
+
+public class test2 {
+	public static void main(String[] args) throws Exception
+	{
+		t6();
+	}
+	static void t1() throws Exception//OK
+	{
+		Executer.execute("stdio<-{print,(4/2)+4};stdio<-{print,(4/2)+14};");
+	}
+	static void t2() throws Exception//OK
+	{
+		Executer.execute("a=4;stdio<-{print,a};");
+	}
+	static void t3() throws Exception //OK
+	{
+		Executer.execute("if(true){stdio<-{print,true};}");
+	}
+	static void t4() throws Exception //OK
+	{
+		Executer.execute("a=10;while(a>1){stdio<-{print,a};a=a-1;}");
+	}
+	static void t5() throws Exception //OK
+	{
+		Executer.execute("stdio<-{print,((1+1)+4)};");
+	}
+	static void t6() throws Exception //OK
+	{
+		Executer.execute("a=5;b=6;stdio<-{print,a+b};");
+	}
+}
